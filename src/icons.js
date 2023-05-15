f2c6004861f41299bb78e370407ddeae2bd84a8e
Brooks Susor <susorbrooks@gmail.com>
2023-05-15 12:23:53 -0400

    Change if statements to switch statement

--- src/icons.js
+++ src/icons.js
@@ -4,25 +4,27 @@ function changeIconShown(weatherData) {
   $('.show-icon').addClass('hide-icon').removeClass('show-icon');
   $('.fa-splotch').addClass('show-icon').removeClass('hide-icon');
 
-  if (weatherData.weather[0].main === 'Clouds') {
-    $('.show-icon').addClass('hide-icon').removeClass('show-icon');
-    $('.fa-cloud').addClass('show-icon').removeClass('hide-icon');
-  }
-
-  if (weatherData.weather[0].main === 'Clear') {
-    $('.show-icon').addClass('hide-icon').removeClass('show-icon');
-    $('.fa-sun').addClass('show-icon').removeClass('hide-icon');
-  }
-
-  if (weatherData.weather[0].main === 'Rain') {
-    $('.show-icon').addClass('hide-icon').removeClass('show-icon');
-    $('.fa-cloud-rain').addClass('show-icon').removeClass('hide-icon');
-  }
-
-  if (weatherData.weather[0].main === 'Haze') {
-    $('.show-icon').addClass('hide-icon').removeClass('show-icon');
-    $('.fa-smog').addClass('show-icon').removeClass('hide-icon');
+  switch (weatherData.weather[0].main) {
+    case 'Clouds':
+      $('.show-icon').addClass('hide-icon').removeClass('show-icon');
+      $('.fa-cloud').addClass('show-icon').removeClass('hide-icon');
+      break;
+    case 'Clear':
+      $('.show-icon').addClass('hide-icon').removeClass('show-icon');
+      $('.fa-sun').addClass('show-icon').removeClass('hide-icon');
+      break;
+    case 'Rain':
+      $('.show-icon').addClass('hide-icon').removeClass('show-icon');
+      $('.fa-cloud-rain').addClass('show-icon').removeClass('hide-icon');
+      break;
+    case 'Haze':
+      $('.show-icon').addClass('hide-icon').removeClass('show-icon');
+      $('.fa-smog').addClass('show-icon').removeClass('hide-icon');
+      break;
+    default:
+      console.log('default');
   }
 }
+
 
 export default changeIconShown;
